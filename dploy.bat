4bdb98e12327e00a8454c1b4868e0c99aca3cdb1
Amey Parundekar <parundekaramey@gmail.com>
2018-05-11 16:46:35 +0530

    Added batch file

---
+++ dploy.bat
@@ -0,0 +1,10 @@
+cd /d %PROJECT_HOME%
+call ant war
+copy %PROJECT_HOME%\\%PROJECT_NAME%.war %CATALINA_HOME%\\webapps
+cd /d %CATALINA_HOME%\\bin
+call startup.bat 
+timeout 5
+start http://localhost:8080/%PROJECT_NAME%
+exit
+
+ 
